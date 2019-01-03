d7331f80d7fad376a9cd1d42b6106d486b97ed91
nysyo <tds24004@gmail.com>
2019-01-04 00:07:37 +0900

    first commit

---
+++ Assets/SerialSample.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerialSample: MonoBehaviour {
+    private SerialPortWrapper _serialPort;
+
+    int p;
+    string s;
+    void Awake() {
+        _serialPort = new SerialPortWrapper("COM5", 9600);
+    }
+
+    private void OnDisable() {
+        _serialPort.KillThread();
+    }
+
+    void Update() {
+        p = (int)Input.mousePosition.x / 4;
+        if (Input.GetMouseButton(0)) {
+            Debug.Log(p);
+            s = p.ToString();
+            _serialPort.Write(s);
+        }
+    }
+}
