1857d17920fa35d5f74ee1058d73c995142bf140
nysyo <tds24004@gmail.com>
2019-01-20 00:31:08 +0900

    Update

---
+++ Assets/connect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class connect : MonoBehaviour {
+    public string portName = "COM5";
+    public int baundRate = 9600;
+    private SerialPort mySerial;
+    // Use this for initialization
+    void Start () {
+        mySerial = new SerialPort(portName, baundRate);
+        mySerial.Open();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A)) {
+            mySerial.Write("a");
+        }
+        if (Input.GetKeyDown(KeyCode.E)) {
+            mySerial.Write("e");
+        }
+		
+	}
+}
